c023656009d508217a32d7d6c0bf4165a935e083
NaKyle Wright <nakyle.wright@gmail.com>
2017-09-27 22:56:13 -0500

    Remove old areas and rename area-1 to bridge.

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 func _ready():
 	set_process_input(true)
-	var area_1 = load("res://scenes/area-1.tscn").instance()
-	area_1.set_name("current-area")
-	add_child(area_1)
+	var bridge = load("res://scenes/bridge.tscn").instance()
+	bridge.set_name("current-area")
+	add_child(bridge)
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
