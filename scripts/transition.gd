527af2dd5a546f2822c6f7d2b82e7ea9292c172f
NaKyle Wright <nakyle.wright@gmail.com>
2018-11-07 22:12:07 -0600

    Add debug logging to teleport script.

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -7,5 +7,6 @@ func _ready():
 	connect("body_entered", self, "body_entered")
 
 func body_entered(body):
+	logger.debug("%s.body_entered(%s)" % [get_name(), body.get_name()])
 	if body.get_name() == "player" and scene:
 		game_state.set_scene(scene, entry_node)
