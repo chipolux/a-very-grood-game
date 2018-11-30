5622eb79969c422ecbb5e3686f8ca52c3a7fd9da
NaKyle Wright <nakyle.wright@gmail.com>
2018-11-30 01:32:08 -0600

    Add cell door scene.

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,24 +1,38 @@
 extends Node2D
 
 
-onready var interaction_key = get_node("interaction_key")
+onready var anim_player = get_node("anim_player")
+
+export(bool) var open = true
+export(bool) var locked = false
 
 
 func _ready():
 	get_node("button").connect("body_entered", self, "body_entered")
 	get_node("button").connect("body_exited", self, "body_exited")
-	interaction_key.hide()
+	if open:
+		anim_player.play("open")
+	else:
+		anim_player.play_backwards("open")
 
 func body_entered(body):
 	if body.get_name() == "player":
-		interaction_key.show()
 		game_state.current_interactable = self
 
 func body_exited(body):
 	if body.get_name() == "player":
 		if game_state.current_interactable == self:
-			interaction_key.hide()
 			game_state.current_interactable = null
 
 func interact():
-	queue_free()
+	if anim_player.is_playing():
+		return
+	if locked:
+		return
+	open = not open
+	if open:
+		anim_player.play("open")
+		get_node("open_audio").play()
+	else:
+		anim_player.play_backwards("open")
+		get_node("close_audio").play()
